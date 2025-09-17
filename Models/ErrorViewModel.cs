250d324f2407e9ad68094e2529d218b8f9b76899
snaki <snaki@SOPHIADELL>
2025-09-17 15:47:29 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2025_Project1_snkyemba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
