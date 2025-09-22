37fc0123a9cc36c3202e428b29a5396372fe316c
Sophia Kyemba <snkyemba@crimson.ua.edu>
2025-09-22 13:21:51 -0500

    Project commit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Actor name to IMDb link mapping
+const actorLinks = {
+    "Daniel Radcliffe": "https://www.imdb.com/name/nm0705356/",
+    "Emma Watson": "https://www.imdb.com/name/nm0914612/",
+    "Rupert Grint": "https://www.imdb.com/name/nm0342488/"
+};
 
-// Write your JavaScript code.
+// Hover and click functionality
+document.querySelectorAll("#Actors li").forEach(actor => {
+    // Click to open IMDb in new tab
+    actor.addEventListener("click", () => {
+        const name = actor.textContent.trim();
+        if (actorLinks[name]) {
+            window.open(actorLinks[name], "_blank");
+        }
+    });
+});
